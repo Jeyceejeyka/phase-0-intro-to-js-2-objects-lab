b84f9d0130dccf0cb068e5613c09bf6b3d9384fd
jeycee <jeyceejeyka635@gmail.com>
2024-11-24 17:02:16 +0300

    Adds new modified object lab with comments and extra info

--- index.js
+++ index.js
@@ -2,36 +2,42 @@
 const employee = {
     name: "Sam",
     streetAddress: "11 Broadway",
+   
   };
   
   
   function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
+    return { ...employee, [key]: value }; 
+
   }
-  
+ // basically returns the original object; 
+  console.log("Original employee after non-destructive update:", employee); 
+
+
+// adding key of position and manager as a value
   const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "position", "Manager");
   console.log("Non-destructive update:", updatedEmployee); 
-  console.log("Original employee after non-destructive update:", employee); 
   
- 
+  
+ //Adding a new key with age as value;
   function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
   }
-  
   destructivelyUpdateEmployeeWithKeyAndValue(employee, "age", 30);
   console.log("Destructive update:", employee); 
   
-  
+  //deletes key and value from the newEmployee object
   function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = { ...employee };
     delete newEmployee[key];
     return newEmployee;
-  }
+  } 
   
+  // returns cloned employee;
   const clonedEmployee = deleteFromEmployeeByKey(employee, "age");
-  console.log("Non-destructive delete:", clonedEmployee); 
-  console.log("Original employee after non-destructive delete:", employee); 
+  console.log("Non-destructive delete:", clonedEmployee);
+  console.log("Original employee after non-destructive delete:", employee);
   
   
   function destructivelyDeleteFromEmployeeByKey(employee, key) {
